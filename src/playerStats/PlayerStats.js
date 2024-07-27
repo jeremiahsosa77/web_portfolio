4856334ace74284b948a1acab7dd62a2cba204ab
jeremiahsosa77 <jeremiahsosa27@gmail.com>
2024-07-27 02:51:07 -0500

    Half Way done!

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1>JEREMIAH SOSA LEVEL 20</h1>
+            <div id="playerStats_lines">
+                <span id="playerStats_lines_thick"></span>
+                <span id="playerStats_lines_thin"></span>
+            </div>
+            <h2>Fullstack Developer and SWE Fellow @Headstarter AI</h2>
+        </div>
+    );
+};
+
+export default PlayerStats;
